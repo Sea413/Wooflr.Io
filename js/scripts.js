f0d0ce7c223af0958b0945d999eb48b9d107f3aa
David Remington and Sean Peerenboom <student@epicodus.com>
2016-02-01 14:38:10 -0800

    Add existing javascript changes and prepare for calander array push.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,13 @@ function Dog(dname,age,dsize) {
   this.dsize = dsize;
 };
 
-function Playdate(time,place) {
+function Playdate(time,place,rng) {
   this.time = time;
   this.place = place;
+  this.rng = 0;
   this.dogs = [];
 }
-function Jerry(jname,jage,jsize) {
+function Jerry(jname,jage,jsize,rng) {
   this.jname = "Gimli";
   this.jage = "15";
   this.jsize = "Obese";
@@ -24,26 +25,51 @@ function Berney(bname,bage,bsize) {
   this.bage = "2";
   this.bsize = "Mediumish";
 };
-function zues (zname,zage,zsize) {
+function Zues (zname,zage,zsize) {
   this.zname = "Apollo";
   this.zage = "6";
   this.zsize = "small";
 };
 
-Dog.prototype.dogInfo = function(){
-  return this.dname + " " + this.age + " " + this.dsize;
+Playdate.prototype.companion = function(){
+  var random = Math.floor((Math.random()*4) + 1);
+  this.rng += random;
+  if (rng === 1) {
+    var newJerry = new Jerry;
+    return newJerry;
+  } else if (rng === 2) {
+    var newWesley = new Wesley;
+    return newWesley;
+    } else if (rng === 3) {
+      var newBerney = new Berney;
+      return newBerney;
+      } else if (rng === 4) {
+        var newZues = new Zues;
+        return newZues;
+        }
+  };
 
-  }
+  Dog.prototype.dogInfo = function(){
+    return this.dname + " " + this.age + " " + this.dsize;
+  };
+
 $(document).ready(function() {
-  $("form#question").submit(function(event) {
+  $("form#dogForm").submit(function(event) {
     event.preventDefault();
     var nameentry = $("input#name").val();
+    var dage = $("select#age").val();
+    var dsize = $("select#size").val();
     var ddate = new Dog(nameentry,dage,dsize);
-    var dage = parseint()"input#age").val();
-    var dsize = $("?????????").val();
-    $("#customern").append(nameentry);
-    $("#size").show();
-    ddate.doginfo();
+    // $("#dinfo").append(nameentry);
+    // $("#dsize").append(dage);
+    // $("#dsize").append(dsize);
+    // $("#confirmForm").show();
+      $("form#confirmForm").submit(function(event) {
+        event.preventDefault();
+
+
+    ddate.dogInfo();
 
      });
      });
+      });
