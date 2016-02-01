83fad3f0d12232f024f92409b7528e0dbcd3663a
David Remington and Sean Peerenboom <student@epicodus.com>
2016-02-01 13:29:23 -0800

    Push to converge existing work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,49 @@
-function People(username,time,neighborhood,activity,comments) {
-  this.username =username;
-  this.time = time;
-  this.neighborhood = neighborhood;
-  this.activity = activity;
-  this.comments = comments;
-  this.score = 0;
-}
+function Dog(dname,age,dsize) {
+  this.dname = dname;
+  this.age = age;
+  this.dsize = dsize;
+};
 
-function Burton(time1,neighborhood2,activity3) {
-  this.time1 = time;
-  this.neighborhood2 = neighborhood;
-  this.activity3 = activity;
+function Playdate(time,place) {
+  this.time = time;
+  this.place = place;
+  this.dogs = [];
 }
-People.prototype.time = function(){
-  if (this.time === this.time1] ) {
-        this.score += 1;
-        return this.score;
-      }
-  else {
-    (this.time !== this.time1){
-      this.score +=-1
-      return this.score;
+function Jerry(jname,jage,jsize) {
+  this.jname = "Gimli";
+  this.jage = "15";
+  this.jsize = "Obese";
+};
+function Wesley(wname,wage,wsize) {
+  this.wname = "Snoop Dog";
+  this.wage = "5";
+  this.wsize = "Fabulous";
+};
+function Berney(bname,bage,bsize) {
+  this.bname = "Sandy";
+  this.bage = "2";
+  this.bsize = "Mediumish";
+};
+function zues (zname,zage,zsize) {
+  this.zname = "Apollo";
+  this.zage = "6";
+  this.zsize = "small";
+};
+
+Dog.prototype.dogInfo = function(){
+  return this.dname + " " + this.age + " " + this.dsize;
+
   }
-}
-People.prototype.neighborhood = function(){
-  if (this.neighborhood === this.neighborhood2] ) {
-        this.score += 1;
-        return this.score;
-      }
-  else {
-    (this.neighborhood !== this.neighborhood2){
-      this.score +=-1
-      return this.score;
-  }
-}
-People.prototype.activity = function(){
-  if (this.activity === this.activity3] ) {
-        this.score += 1;
-        return this.score;
-      }
-  else {
-    (this.activity === this.activity3){
-      this.score +=-1
-      return this.score;
-  }
-}
+$(document).ready(function() {
+  $("form#question").submit(function(event) {
+    event.preventDefault();
+    var nameentry = $("input#name").val();
+    var ddate = new Dog(nameentry,dage,dsize);
+    var dage = parseint()"input#age").val();
+    var dsize = $("?????????").val();
+    $("#customern").append(nameentry);
+    $("#size").show();
+    ddate.doginfo();
+
+     });
+     });
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,23 @@
-describe("People", function(){
-  it("Determines existing customer value", function(){
-  var testObject = new Customer ("Jerry","Trifling","Kale");
-  expect(testObject.name).to.equal("Jerry");
-  expect(testObject.psize).to.equal("Trifling");
-  expect(testObject.topping).to.equal("Kale");
+describe("Dogs", function(){
+  it("stores info on users's dog", function(){
+  var testObject = new Dog ("Jerry","12","large");
+  expect(testObject.dname).to.equal("Jerry");
+  expect(testObject.age).to.equal("12");
+  expect(testObject.dsize).to.equal("large");
+});
+it("Attempt to verify if the prototype is working", function(){
+var testObject = new Dog ("Jerry","12","large");
+expect(testObject.dname).to.equal("Jerry");
+expect(testObject.age).to.equal("12");
+expect(testObject.dsize).to.equal("large");
+expect(testObject.dogInfo()).to.equal("Jerry 12 large");
+});
+});
+describe("Playdate", function() {
+  it("stores info on playdate time, place, and attending dogs", function(){
+  var testObject = new Playdate ("10am","Forest Park");
+  expect(testObject.time).to.equal("10am");
+  expect(testObject.place).to.equal("Forest Park")
+  expect(testObject.dogs).to.eql([]);
 });
 });
