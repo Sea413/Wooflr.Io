eaf9b3a3a3dd61bc408bc92043206937a4895114
sea413 <student@epicodus.com>
2016-02-04 10:19:07 -0800

    provide a copy for david

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,116 @@
-
-function Dog(dname,dage,dsize,imgtag) {
+function Dog(dname,dage,dsize,dtime,imgtag,score) {
   this.dname = dname;
   this.dage = dage;
   this.dsize = dsize;
+  this.dtime =dtime;
   this.imgtag = imgtag;
   this.appendvar = [];
+  this.score = 0;
 };
 
-Dog.prototype.nameselect = function(){
-  if (this.dsize === database.dogs[0].dsize) {
-    this.appendvar.push(database.dogs[0]);
-  } else if (this.dsize === database.dogs[1].dsize) {
-      this.appendvar.push(database.dogs[1]);
-  } else if (this.dsize === database.dogs[2].dsize) {
-      this.appendvar.push(database.dogs[2]);
-  } else if (this.dsize === database.dogs[3].dsize) {
-      this.appendvar.push(database.dogs[3]);
-  } else if (this.dsize === database.dogs[4].dsize) {
-      this.appendvar.push(database.dogs[4]);
+Dog.prototype.sizeselect = function(){
+if (this.dsize === database.dogs[0].dsize) {
+  this.score += 1;
+} else if (this.dsize === database.dogs[1].dsize ) {
+  this.score += 2;
+} else if (this.dsize === database.dogs[2].dsize) {
+    this.score += 3;
+}else if (this.dsize === database.dogs[3].dsize) {
+    this.score += 4;
+}else if (this.dsize === database.dogs[4].dsize) {
+    this.score += 5;
   }
 };
+Dog.prototype.ageselect = function(){
+if (this.dage <= 2 && this.dage > 0 ) {
+  this.score += 1;
+} else if (this.dage <= 5 && this.dage >2 ) {
+  this.score += 2;
+} else if (this.dage <= 6 && this.dage >5 ) {
+    this.score += 3;
+}else if (this.dage <= 9 && this.dage > 6) {
+    this.score += 4;
+}else if (this.dage <= 15 && this.dage > 9) {
+    this.score += 5;
+  }else if (this.dage > 15) {
+      this.score += 9000;
+    }
+};
+// Dog.prototype.timeselect = function(){
+// if (this.dtime === database.dogs[0].dtime) {
+//   this.score += 5;
+// } else if (this.dtime === database.dogs[1].dtime ) {
+//   this.score += 3;
+// } else if (this.dtime === database.dogs[2].dtime) {
+//     this.score += 5;
+// }else if (this.dtime === database.dogs[3].dtime) {
+//     this.score += 5;
+// }else if (this.dtime === database.dogs[4].dtime) {
+//     this.score += 3;
+//   }
+//   };
 
+Dog.prototype.nameselect = function(){
+  if (this.score > 0 && this.score <= 2 ) {
+    this.appendvar.push(database.dogs[0]);
+  } else if (this.score > 2 && this.score <= 4) {
+      this.appendvar.push(database.dogs[1]);
+  } else if (this.score >4 && this.score <= 6) {
+      this.appendvar.push(database.dogs[2]);
+  } else if (this.score > 6 && this.score <= 8) {
+      this.appendvar.push(database.dogs[3]);
+  } else if (this.score > 8 && this.score <= 10) {
+      this.appendvar.push(database.dogs[4]);
+  }else if (this.score > 10 && this.score <= 10000) {
+      this.appendvar.push(database.dogs[5]);
+    }
+};
   // Fake JSON Database //
-
 var database = {
   "dogs": [
       {
           "dname": "Gimli",
           "dage": "15",
-          "dsize": "x-large",
+          "dsize": "X-large",
+          "dtime": "Morning",
           "imgtag": "#gimliimg"
       },
       {
           "dname": "Snoop Dog",
           "dage": "5",
-          "dsize": "small",
+          "dsize": "Small",
+          "dtime": "Afternoon",
           "imgtag": "#snoopimg"
       },
       {
           "dname": "Sandy",
           "dage": "2",
-          "dsize": "x-small",
+          "dsize": "X-small",
+          "dtime": "Evening",
           "imgtag": "#sandyimg"
       },
       {
           "dname": "Apollo",
           "dage": "6",
-          "dsize": "large",
+          "dsize": "Large",
+          "dtime": "Morning",
           "imgtag": "#apolloimg"
       },
       {
           "dname": "Samuel",
           "dage": "9",
-          "dsize": "medium",
+          "dsize": "Medium",
+          "dtime": "Afternoon",
           "imgtag": "#samuelimg"
+      },
+      {
+          "dname": "Charlie, Destroyer of Worlds", //7//
+          "dage": "968",
+          "dsize": "x-small",
+          "imgtag": "#charlieimg"
       }
   ]
-}
+};
   // End Fake Database //
 
 $(function () {
@@ -78,9 +132,12 @@ $(document).ready(function() {
     var newdname = $("input#name").val();
     var newdage = $("select#age").val();
     var newdsize = $("select#size").val();
-    var dtime = $("select#time").val();
+    var newdtime = $("select#time").val();
     var dcalendar = $("input#inputCalendar").val();
-    var newDog = new Dog(newdname,newdage,newdsize,0);
+    var newDog = new Dog(newdname,newdage,newdsize,newdtime,0);
+    newDog.sizeselect();
+    newDog.ageselect();
+    // newDog.timeselect();
     newDog.nameselect();
 
     var selector = newDog.appendvar[0].imgtag.toString();
@@ -91,10 +148,19 @@ $(document).ready(function() {
     $("#dname").html(newDog.dname);
     $("#dage").html(newDog.dage);
     $("#dsize").html(newDog.dsize);
-    $("#dtime").html(dtime);
+    $("#dtime").html(newdtime);
     $("#dcalendar").html(dcalendar);
     $("#match-dname").html(newDog.appendvar[0].dname);
     $("#match-dage").html(newDog.appendvar[0].dage);
     $("#match-dsize").html(newDog.appendvar[0].dsize);
+    $("#newForm").show();
+    $("#dogForm").show();
+    $("#submitButton").hide();
   });
+  });
+  $("form#newForm").submit(function(event) {
+    event.preventDefault();
+    $("#dogForm").hide();
+    $("#submitButton").show();
+
 });
